f2470cacf3c3e8628f147fb2acff0011a1788709
Tomas Kucera <tom4f@seznam.cz>
2021-11-13 17:59:37 +0100

    ISR correction2

--- pages/[page].js
+++ pages/[page].js
@@ -2,42 +2,46 @@ import fs from 'fs'
 import path from 'path'
 import { OneGraph }     from './../components/OneGraph'
 import Meta             from './../components/Meta' 
+import { Fragment } from 'react'
 
-export default function Home( { graphsData } ) {
+export default function Home( { graphsConfig, respAll } ) {
+    const respAllFulfilled = respAll.map( one => one.status === 'fulfilled' ? one.value.data : '' )
+    const graphsData = respAllFulfilled.map( (value, index) => ( { data: value, ...graphsConfig[index] } ) )
+    const metaDesc = graphsData.map( graph => {
+        const specificHeader  = graph.specific.map(   spec => spec.header ).join( ', ' )
+        const specificHeader2 = graph.specific2 ? graph.specific2.map( spec => spec.header ).join( ', ' ) : ''
+        return `${specificHeader}, ${specificHeader2}`
+    })
+
     return (
         <>
-            <Meta title="Covid Test Graphs Czech Republic" description="daily updated covid testing" />
-            { graphsData.map( (graphData, index) =>
-                graphData.data && <OneGraph key={index} graphData={graphData}/>
-            )}
-
-            {graphsData.map( (graphData, index) =>
-                graphData.specific2 && <OneGraph key={index} graphData={ (
-                    {
-                        ...graphsData[index],
-                        specific: graphData.specific2
-                    }
-                ) }/>)
+            <Meta title={ graphsData[0].common.title } description={ metaDesc.join( ', ' ) } />
+            {
+                graphsData.map( (graphData, index) => graphData.data && (
+                        <Fragment key={index}>
+                            <OneGraph key={index--} graphData={graphData}/>
+                            { graphData.specific2 && <OneGraph key={index} graphData={ ( { ...graphData, specific: graphData.specific2  } ) }/> }
+                        </Fragment>
+                    )
+                )
             }
         </>
     )
 }
 
-
 export async function getStaticPaths() {
     const files = fs.readdirSync(path.join('config'))
-  
     const paths = files.map((filename) => ({
       params: {
         page: filename.replace('.json', ''),
       },
     }))
-  
+
     return {
-      paths,
-      fallback: false,
+        paths,
+        fallback: false,
     }
-  }
+}
 
 export async function getStaticProps( { params: { page } } ) {
 
@@ -45,14 +49,13 @@ export async function getStaticProps( { params: { page } } ) {
     const graphsConfig = JSON.parse( graphsJson )
     const urlList = graphsConfig.map( value => value.common.url )
     const fetchList = urlList.map( url => fetch( url ).then( resp => resp.json() )  )
-    
-    const respAll = await Promise.allSettled( fetchList )
-    
-    const respAllFulfilled = respAll.map( one => one.status === 'fulfilled' ? one.value.data : '' )
-    const graphsData = respAllFulfilled.map( (value, index) => ( { data: value, ...graphsConfig[index] } ) )
+    const respAll = await Promise.allSettled( fetchList )  
 
     return {
-        props: { graphsData },
+        props: {
+            graphsConfig,
+            respAll
+        },
         revalidate: 11,
     }
 }
