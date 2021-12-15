a38a027981dc1721664fc372f3d66e367c72a660
Tomas Kucera <tom4f@seznam.cz>
2021-12-15 11:55:28 +0100

    cleaning

--- components/OneGraph.tsx
+++ components/OneGraph.tsx
@@ -1,4 +1,4 @@
-import React, { useRef, useEffect } from 'react';
+import { useRef, useEffect } from 'react';
 import Draw from './Draw';
 import oneGraphStyles from '../styles/OneGraph.module.scss'
 import { OneGraphType, showGraphType } from './TypeDefinition';
@@ -8,13 +8,10 @@ export const OneGraph = ( { graphData }: OneGraphType ) => {
     const graphHeight        = 300;
     const canvasRef          = useRef<HTMLCanvasElement | null>( null );
     const canvas_pointerRef  = useRef<HTMLCanvasElement | null>( null );
-
     const loadPocasiAsync = () => {
         const canvas         = canvasRef.current as HTMLCanvasElement;
         const canvas_pointer = canvas_pointerRef.current as HTMLCanvasElement;
-        
         const newDraw = new Draw( canvas, canvas_pointer, graphData ); 
-
         const showGraph: showGraphType = ( canvas, canvas_pointer, graphHeight ) => {
             const clientWidth  = document.documentElement.clientWidth;
             //const clientHeight = document.documentElement.clientHeight;
@@ -24,20 +21,16 @@ export const OneGraph = ( { graphData }: OneGraphType ) => {
             canvas_pointer.height = graphHeight;    
             newDraw.graph();
         }
-
         canvas && canvas_pointer && showGraph(canvas, canvas_pointer, graphHeight)
-
         window.addEventListener( 'resize', () =>
             canvas && canvas_pointer && showGraph( canvas, canvas_pointer, graphHeight )
-        )
-    }
-
-    useEffect( loadPocasiAsync, [ graphData ] );
-
-    return (
+            )
+        }
+        useEffect( loadPocasiAsync, [ graphData ] );
+        return (
             <article className = { oneGraphStyles.allGraphs } >
                 <canvas ref = { canvasRef } className = { oneGraphStyles.canvas } />
                 <canvas ref = { canvas_pointerRef } className = { oneGraphStyles.canvas_pointer } />
             </article>
-    )
+        )
 }
